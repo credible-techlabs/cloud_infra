a6f59adf5ff8c93832948bfce72301b7d8f79e63
Ashish Arora <ashisharora24@outlook.com>
2024-09-24 07:12:19 +0530

    fix: removing storage account creation from here

--- deploy/dev/main.tf
+++ deploy/dev/main.tf
@@ -1,20 +1,3 @@
 provider "azurerm" {
   features {}
 }
-resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
-}
