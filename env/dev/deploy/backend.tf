476c43fb7580f6de8a3c04be4d595629ee01d5e1
Ashish Arora <ashisharora24@outlook.com>
2024-09-24 13:08:51 +0530

    fix: moving the backend details to github secrets

--- env/dev/deploy/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "medx_base"
-    storage_account_name  = "bootstrapdev0001"
-    container_name        = "terraform"
-    key                   = "terraform.tfstate"
-  }
-}
