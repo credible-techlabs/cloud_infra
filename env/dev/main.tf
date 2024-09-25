e295901a43c6182fbda6d73c691cf8698af5d3ad
Ashish Arora <ashisharora24@outlook.com>
2024-09-25 13:39:53 +0530

    fix: revert to non module

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,21 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-# Call the Resource Group module
-module "resource_group" {
-  source   = "../../terraform/modules/resource_group"
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# # Call the Virtual Network module
-# module "virtual_network" {
-#   source              = "../../terraform/modules/virtual_network"
-#   vnet_name           = var.vnet_name
-#   address_space       = ["10.0.0.0/16"]
-#   location            = "eastus"
-#   resource_group_name = module.resource_group.resource_group_name
-#   subnet_name         = var.subnet_name
-#   subnet_prefixes     = ["10.0.1.0/24"]
-# }
