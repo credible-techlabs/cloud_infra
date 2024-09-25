93d4b0d1e5aae955d29ab160abe3771fd2634f4f
Ashish Arora <ashisharora24@outlook.com>
2024-09-25 14:02:03 +0530

    test002

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -3,22 +3,22 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "example"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.address_prefixes
 
   delegation {
     name = "delegation"
--- env/dev/terraform.tfvars
+++ env/dev/terraform.tfvars
@@ -1,6 +1,6 @@
 resource_group_name  = "infra_Dev"
 location             = "East US"
 vnet_name            = "myvnet"
+address_space        = ["10.0.0.0/16"]
 subnet_name          = "mysubnet"
-storage_account_name = "medxdev0001"
-container_name       = "devterraform"
+address_prefixes     = ["10.0.1.0/24"]
